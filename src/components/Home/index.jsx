513ccf5687e1c581eba8e10408aab92bf6fbb4a0
ErleyDiangelys <36991795+erleydiangelys@users.noreply.github.com>
2022-03-18 17:08:42 -0300

    colocando paginação

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,10 +3,14 @@ import './style.css';
 
 import { Posts } from '../../components/posts';
 import { loadPost } from '../../utils/load-posts';
+import { Button } from '../Button';
 
 class Home extends Component {
   state = {
-  posts: []
+  posts: [],
+  allPosts: [],
+  page: 0,
+  postPPerPage: 3,
 };
 
   
@@ -16,16 +20,42 @@ class Home extends Component {
   }
 
   loadPosts = async () => {
+    const {page, postPPerPage} = this.state;
     const postAndPhotos = await loadPost();
-    this.setState({posts: postAndPhotos});
+    this.setState({
+      posts: postAndPhotos.slice(page, postPPerPage),
+      allPosts: postAndPhotos,
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postPPerPage,
+      allPosts,
+      posts
+    } = this.state;
+    const nextPage = page + postPPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({posts, page: nextPage})
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postPPerPage, allPosts } = this.state;
+    const noMorePost = page + postPPerPage >= allPosts.length;
 
     return (
       <section className="container">
         <Posts posts={posts}/>
+
+        <div className="button-container">
+          <Button text="Load more posts"
+                  acao={this.loadMorePosts}
+                  disabled={noMorePost}
+          />
+        </div>
       </section>
     );
   }
