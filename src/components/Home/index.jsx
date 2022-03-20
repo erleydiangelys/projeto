3a11b76d189e76c463144ec573c6fa49bc71ffe1
ErleyDiangelys <36991795+erleydiangelys@users.noreply.github.com>
2022-03-19 21:28:34 -0300

    Migrando para Hooks

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import './style.css';
 
 import { Posts } from '../../components/posts';
@@ -6,55 +6,17 @@ import { loadPost } from '../../utils/load-posts';
 import { Button } from '../Button';
 import { TextInput } from '../TextInput';
 
-class Home extends Component {
-  state = {
-  posts: [],
-  allPosts: [],
-  page: 0,
-  postPPerPage: 4,
-  seacheValue: '',
-};
+export const Home = () => {
 
-  
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPostss] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postPPerPage] = useState(4);
+  const [seacheValue, setSeacheValue] = useState('');
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
+  const noMorePost = page + postPPerPage >= allPosts.length;
 
-  loadPosts = async () => {
-    const {page, postPPerPage} = this.state;
-    const postAndPhotos = await loadPost();
-    this.setState({
-      posts: postAndPhotos.slice(page, postPPerPage),
-      allPosts: postAndPhotos,
-    });
-  }
-
-  loadMorePosts = () => {
-    const {
-      page,
-      postPPerPage,
-      allPosts,
-      posts
-    } = this.state;
-    const nextPage = page + postPPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postPPerPage);
-    posts.push(...nextPosts);
-
-    this.setState({posts, page: nextPage})
-  }
-
-  handleChange = (e) => {
-    const {value} = e.target;
-    this.setState({seacheValue: value});
-  }
-
-
-  render() {
-    const { posts, page, postPPerPage, allPosts, seacheValue } = this.state;
-    const noMorePost = page + postPPerPage >= allPosts.length;
-
-    const filteredPost = !!seacheValue ? 
+  const filteredPost = !!seacheValue ? 
     allPosts.filter(post => {
       return post.title.toLowerCase().includes(
         seacheValue.toLowerCase()
@@ -63,14 +25,42 @@ class Home extends Component {
     
     : posts;
 
-    return (
-      <section className="container">
+    
+  const handleloadPost = useCallback(async (page, postPPerPage) => {
+    const postAndPhotos = await loadPost();
+
+    setPosts(postAndPhotos.slice(page, postPPerPage));
+    setAllPostss(postAndPhotos);
+  }, []);
+
+  useEffect(()=> {
+    handleloadPost(0, postPPerPage);
+  }, [handleloadPost, postPPerPage]);
+
+  const loadMorePosts = () => {
+   
+    const nextPage = page + postPPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPPerPage);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  }
+
+  const handleChange = (e) => {
+    const {value} = e.target;
+    setSeacheValue(value)
+  }
+
+
+  return (
+    <section className="container">
         <div className="search-container">
           {!!seacheValue && (
               <h1> Resultados para: {seacheValue} </h1>
           )}
 
-          <TextInput searchValue={seacheValue} handleChange={this.handleChange} />
+          <TextInput searchValue={seacheValue} handleChange={handleChange} />
         </div>   
 
          {filteredPost.length > 0 && (
@@ -83,14 +73,100 @@ class Home extends Component {
         <div className="button-container">
           {!seacheValue && (
             <Button text="Carregar mais posts"
-            acao={this.loadMorePosts}
+            acao={loadMorePosts}
             disabled={noMorePost}
         />
           )}
         </div>
       </section>
-    );
-  }
+  )
 }
+
+// class Home2 extends Component {
+//   state = {
+//   posts: [],
+//   allPosts: [],
+//   page: 0,
+//   postPPerPage: 4,
+//   seacheValue: '',
+// };
+
+  
+
+//   async componentDidMount() {
+//     await this.loadPosts();
+//   }
+
+//   loadPosts = async () => {
+//     const {page, postPPerPage} = this.state;
+//     const postAndPhotos = await loadPost();
+//     this.setState({
+//       posts: postAndPhotos.slice(page, postPPerPage),
+//       allPosts: postAndPhotos,
+//     });
+//   }
+
+//   loadMorePosts = () => {
+//     const {
+//       page,
+//       postPPerPage,
+//       allPosts,
+//       posts
+//     } = this.state;
+//     const nextPage = page + postPPerPage;
+//     const nextPosts = allPosts.slice(nextPage, nextPage + postPPerPage);
+//     posts.push(...nextPosts);
+
+//     this.setState({posts, page: nextPage})
+//   }
+
+//   handleChange = (e) => {
+//     const {value} = e.target;
+//     this.setState({seacheValue: value});
+//   }
+
+
+//   render() {
+//     const { posts, page, postPPerPage, allPosts, seacheValue } = this.state;
+//     const noMorePost = page + postPPerPage >= allPosts.length;
+
+//     const filteredPost = !!seacheValue ? 
+//     allPosts.filter(post => {
+//       return post.title.toLowerCase().includes(
+//         seacheValue.toLowerCase()
+//       );
+//     })
+    
+//     : posts;
+
+//     return (
+//       <section className="container">
+//         <div className="search-container">
+//           {!!seacheValue && (
+//               <h1> Resultados para: {seacheValue} </h1>
+//           )}
+
+//           <TextInput searchValue={seacheValue} handleChange={this.handleChange} />
+//         </div>   
+
+//          {filteredPost.length > 0 && (
+//             <Posts posts={filteredPost}/>
+//          )} 
+//           {filteredPost.length === 0 && (
+//             <p>Não existes postes para sua busca</p>
+//          )} 
+
+//         <div className="button-container">
+//           {!seacheValue && (
+//             <Button text="Carregar mais posts"
+//             acao={this.loadMorePosts}
+//             disabled={noMorePost}
+//         />
+//           )}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
 
 export default Home;
